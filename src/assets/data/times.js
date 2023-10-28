50e2db9dcd6b1ed9c35afe8e72b4807b41a13160
Cyril <cyril.personne@outlook.fr>
2023-10-28 17:50:16 +0200

    🍱 Add working hours data

---
+++ src/assets/data/times.js
@@ -0,0 +1,37 @@
+export const times = [
+    {
+        id: 1,
+        day: "Lundi",
+        hours: "Fermé"
+    },
+    {
+        id: 2,
+        day: "Mardi",
+        hours: "08:30 - 19:00",
+    },
+    {
+        id: 3,
+        day: "Mercredi",
+        hours: "08:30 - 19:00",
+    },
+    {
+        id: 4,
+        day: "Jeudi",
+        hours: "08:30 - 19:00",
+    },
+    {
+        id: 5,
+        day: "Vendredi",
+        hours: "08:30 - 19:00",
+    },
+    {
+        id: 6,
+        day: "Samedi",
+        hours: "08:30 - 19:00",
+    },
+    {
+        id: 7,
+        day: "Dimanche",
+        hours: "Fermé",
+    }
+];
