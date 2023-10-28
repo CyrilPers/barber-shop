1b5bcdcb10bb453f5e7017a997b6a53d1376971e
Cyril <cyril.personne@outlook.fr>
2023-10-28 09:16:02 +0200

    ♻️ edit menu

---
+++ src/assets/data/menu.js
@@ -0,0 +1,108 @@
+export const menu = {
+  FORFAITS: [
+    {
+      id: 1,
+      name: "Forfait Coupe + barbe",
+      description: "coupe classique + barbe classique",
+      temps: "45mins",
+      prix: "28€"
+    },
+    {
+      id: 2,
+      name: "Forfait PREMIUM",
+      description: "coupe premium + barbe Premium + soin visage premium",
+      temps: "1h35",
+      prix: "80€"
+    }
+  ],
+  COIFFURE: [
+    {
+      id: 3,
+      name: "coupe classique",
+      description: "diagnostic, coiffure et coiffage",
+      temps: "30mins",
+      prix: "20€"
+    },
+    {
+      id: 4,
+      name: "Coupe premium",
+      description: "diagnostic, coiffure, soins cheveux et coiffage",
+      temps: "40mins",
+      prix: "30€"
+    },
+    {
+      id: 5,
+      name: "Coupe -18ans",
+      description: "coupe classique",
+      temps: "30mins",
+      prix: "18€"
+    },
+    {
+      id: 6,
+      name: "Coupe au ciseaux",
+      description: "diagnostic, coiffure uniquement au ciseaux et coiffage",
+      temps: "40mins",
+      prix: "27€"
+    },
+    {
+      id: 7,
+      name: "Design Couleur",
+      description: "",
+      temps: "30mins",
+      prix: "Sur devis"
+    }
+  ],
+  BARBE: [
+    {
+      id: 8,
+      name: "Barbe classique",
+      description: "taillage, finition coupe-choux sous jet de vapeur, serviette",
+      temps: "30mins",
+      prix: "30€"
+    },
+    {
+      id: 9,
+      name: "Barbe PREMIUM",
+      description: "Taille, scultage, finition coupe-choux sous jet de vapeur, serviette",
+      temps: "30mins",
+      prix: "30€"
+    },
+    {
+      id: 10,
+      name: "Rasage Traditionnel",
+      description: "Rasage intégral à l'ancienne sous jet de vapeur et serviette chaud",
+      temps: "40mins",
+      prix: "35€"
+    },
+    {
+      id: 11,
+      name: "Coloration",
+      description: "",
+      temps: "1h",
+      prix: "Sur devis"
+    }
+  ],
+  SOINS: [
+    {
+      id: 12,
+      name: "Soins visage classique",
+      description: "Nettoyage avec brosse et application du black mask",
+      temps: "15mins",
+      prix: "12€"
+    },
+    {
+      id: 13,
+      name: "Soin visage premium",
+      description: "Nettoyage avec brosse, gommage, hydratation du visage sous jet de vapeur",
+      temps: "25mins",
+      prix: "30€"
+    },
+    {
+      id: 14,
+      name: "Soins cheveux",
+      description: "Shampoing et soins adapté à vos cheveux",
+      temps: "15mins",
+      prix: "12€"
+    }
+  ]
+};
