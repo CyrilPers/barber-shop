adf6e7bf90c068d3e81f7f89e1fc75a4eb06f3a4
Cyril <cyril.personne@outlook.fr>
2023-10-29 17:47:04 +0100

    🍱 Review data

---
+++ src/assets/data/reviews.js
@@ -0,0 +1,297 @@
+export const reviews = [
+    {
+        id: 1,
+        date: '14/07/2022',
+        rating: 5,
+        description: 'Un coiffeur fantastique !'
+    },
+    {
+        id: 2,
+        date: '02/09/2022',
+        rating: 5,
+        description: 'Je suis ravi de leur service.'
+    },
+    {
+        id: 3,
+        date: '28/03/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 4,
+        date: '05/11/2022',
+        rating: 5,
+        description: 'Les meilleurs coupes de cheveux en ville.'
+    },
+    {
+        id: 5,
+        date: '17/05/2022',
+        rating: 5,
+        description: 'Service impeccable.'
+    },
+    {
+        id: 6,
+        date: '23/01/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 7,
+        date: '09/10/2022',
+        rating: 5,
+        description: 'Toujours un excellent travail.'
+    },
+    {
+        id: 8,
+        date: '31/08/2022',
+        rating: 5,
+        description: 'Je recommande vivement ce coiffeur.'
+    },
+    {
+        id: 9,
+        date: '19/06/2022',
+        rating: 5,
+        description: 'Un service exceptionnel.'
+    },
+    {
+        id: 10,
+        date: '03/04/2022',
+        rating: 5,
+        description: 'Des professionnels talentueux.'
+    },
+    {
+        id: 11,
+        date: '08/12/2022',
+        rating: 5,
+        description: 'Toujours au top de leur jeu.'
+    },
+    {
+        id: 12,
+        date: '25/02/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 13,
+        date: '14/10/2022',
+        rating: 5,
+        description: 'Le meilleur salon de coiffure.'
+    },
+    {
+        id: 14,
+        date: '16/09/2022',
+        rating: 5,
+        description: 'Des coupes modernes et tendances.'
+    },
+    {
+        id: 15,
+        date: '12/01/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 16,
+        date: '07/06/2022',
+        rating: 5,
+        description: 'Un vrai plaisir de venir ici.'
+    },
+    {
+        id: 17,
+        date: '30/05/2022',
+        rating: 5,
+        description: 'Ils savent ce qu\'ils font.'
+    },
+    {
+        id: 18,
+        date: '21/03/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 19,
+        date: '18/08/2022',
+        rating: 5,
+        description: 'Jamais déçu de leur prestation.'
+    },
+    {
+        id: 20,
+        date: '27/11/2022',
+        rating: 5,
+        description: 'Des professionnels passionnés.'
+    },
+    {
+        id: 21,
+        date: '20/10/2022',
+        rating: 5,
+        description: 'Toujours un excellent service.'
+    },
+    {
+        id: 22,
+        date: '10/04/2022',
+        rating: 5,
+        description: 'Des coupes toujours impeccables.'
+    },
+    {
+        id: 23,
+        date: '01/03/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 24,
+        date: '06/09/2022',
+        rating: 5,
+        description: 'Je recommande vivement ce salon.'
+    },
+    {
+        id: 25,
+        date: '15/08/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 26,
+        date: '04/05/2022',
+        rating: 5,
+        description: 'Des coiffeurs talentueux.'
+    },
+    {
+        id: 27,
+        date: '22/02/2022',
+        rating: 5,
+        description: 'Service de haute qualité.'
+    },
+    {
+        id: 28,
+        date: '29/12/2022',
+        rating: 5,
+        description: 'Une expérience exceptionnelle.'
+    },
+    {
+        id: 29,
+        date: '07/07/2022',
+        rating: 5,
+        description: 'Des coupes qui durent.'
+    },
+    {
+        id: 30,
+        date: '03/01/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 31,
+        date: '09/11/2022',
+        rating: 5,
+        description: 'Des coiffeurs à l\'écoute.'
+    },
+    {
+        id: 32,
+        date: '18/10/2022',
+        rating: 5,
+        description: 'Toujours un plaisir de venir.'
+    },
+    {
+        id: 33,
+        date: '26/06/2022',
+        rating: 5,
+        description: 'Ils savent ce qu\'ils font.'
+    },
+    {
+        id: 34,
+        date: '12/04/2022',
+        rating: 5,
+        description: 'Des experts en coiffure.'
+    },
+    {
+        id: 35,
+        date: '24/08/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 36,
+        date: '05/03/2022',
+        rating: 5,
+        description: 'Un salon au top.'
+    },
+    {
+        id: 37,
+        date: '31/01/2022',
+        rating: 5,
+        description: 'Les meilleurs coiffeurs de la ville.'
+    },
+    {
+        id: 38,
+        date: '13/09/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 39,
+        date: '10/07/2022',
+        rating: 5,
+        description: 'Un service irréprochable.'
+    },
+    {
+        id: 40,
+        date: '17/05/2022',
+        rating: 5,
+        description: 'Des professionnels de confiance.'
+    },
+    {
+        id: 41,
+        date: '20/03/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 42,
+        date: '03/08/2022',
+        rating: 5,
+        description: 'Les meilleurs conseils en coiffure.'
+    },
+    {
+        id: 43,
+        date: '08/06/2022',
+        rating: 5,
+        description: 'Toujours à la pointe des tendances.'
+    },
+    {
+        id: 44,
+        date: '14/02/2022',
+        rating: 4,
+        description: ''
+    },
+    {
+        id: 45,
+        date: '11/10/2022',
+        rating: 5,
+        description: 'Une équipe de professionnels.'
+    },
+    {
+        id: 46,
+        date: '23/09/2022',
+        rating: 5,
+        description: 'Des coiffeurs talentueux et créatifs.'
+    },
+    {
+        id: 47,
+        date: '30/04/2022',
+        rating: 5,
+        description: 'Service rapide et efficace.'
+    },
+    {
+        id: 48,
+        date: '27/07/2022',
+        rating: 5,
+        description: 'Jamais déçu de leur prestation.'
+    },
+    {
+        id: 49,
+        date: '22/11/2022',
+        rating: 5,
+        description: 'Un salon qui sait prendre soin de ses clients.'
+    }
+];
+
