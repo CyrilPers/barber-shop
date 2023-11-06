50629953b5e721e7db1c3a45c724093a5b6bda7e
Cyril <cyril.personne@outlook.fr>
2023-11-06 21:40:43 +0100

    🍱 Add barbers list

---
+++ src/assets/data/barbers.js
@@ -0,0 +1,14 @@
+export const barbers = [
+    {
+        id: 1,
+        name: "Yvan",
+    },
+    {
+        id: 2,
+        name: "Hervé",
+    },
+    {
+        id: 3,
+        name: "Nicolas",
+    },
+]
