1793f538fda68b43361542f469bbf37b552b20ef
Cyril <cyril.personne@outlook.fr>
2023-11-06 21:26:49 +0100

    🚧 Choose barber section

---
+++ src/components/booking/ChooseBarber.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function ChooseBarber() {
+    return (
+        <ChooseBarberStyled>
+            ChooseBarber
+        </ChooseBarberStyled>
+    )
+}
+
+const ChooseBarberStyled = styled.div`
+  background-color: white;
+  
+`;
