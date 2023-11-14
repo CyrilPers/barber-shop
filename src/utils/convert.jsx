5c015a767b8389c9324f87642ae1e239b19b1777
Cyril <cyril.personne@outlook.fr>
2023-11-14 08:00:08 +0100

    ✏️ Remove not needed line

--- src/utils/convert.jsx
+++ src/utils/convert.jsx
@@ -10,10 +10,8 @@ export const convertTime = (time) => {
 export const convertDate = (date) => {
     const dateObj = new Date(date);
     let year = dateObj.getFullYear()
-    // Check if date and month have 1 figure number, if have 1 figure we add 0.
+    // Check if date and month have 1 figure number, if have 1 figure we add 0 at start to get 2 figures number.
     let month = (dateObj.getMonth() + 1).toString().length === 1 ? (dateObj.getMonth() + 1).toString().padStart(2, '0') : dateObj.getMonth() + 1;
     let day = dateObj.getDate().toString().length === 1 ? dateObj.getDate().toString().padStart(2, '0') : dateObj.getDate();
     return day + "/" + month + "/" + year
-
-    // let day = dateObj.getDay().toString().length === 1 ? dateObj.getDay().toString().padStart(2, '0') : dateObj.getDay().toString();
 }
