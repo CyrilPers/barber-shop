e0196fc75757bcbe4170adb77e105ed07642cd2f
Cyril <cyril.personne@outlook.fr>
2023-11-15 08:56:02 +0100

    ♻️

---
+++ src/components/pages/booking/selectedService/ChooseBarber.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import BarberList from '../BarberList';
+import SelectedService from './SelectedService';
+import styled from 'styled-components';
+import { theme } from '../../../../theme';
+
+export default function ChooseBarber({ selectedBarber, setSelectedBarber, setSelectedService, selectedService }) {
+
+    const handleClick = (barber) => {
+        setSelectedBarber(barber)
+    }
+
+    return (
+        <ChooseBarberStyled>
+            <SelectedService selectedService={selectedService} setSelectedService={setSelectedService} />
+            <span className='who'>Avec qui ?</span>
+            <BarberList selectedBarber={selectedBarber} handleClick={handleClick} />
+        </ChooseBarberStyled>
+    )
+}
+
+const ChooseBarberStyled = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+box-shadow: ${theme.shadows.medium};
+box-sizing: border-box;
+padding: 20px 20px;
+background-color: white;
+border-radius: ${theme.borderRadius.midRound};
+`;
