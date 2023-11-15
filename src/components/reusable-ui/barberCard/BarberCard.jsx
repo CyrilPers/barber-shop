3eab1b30d30dd37025c6de670e9bf8d5f236339a
Cyril <cyril.personne@outlook.fr>
2023-11-15 08:56:45 +0100

    ✅ Select barber

--- src/components/reusable-ui/barberCard/BarberCard.jsx
+++ src/components/reusable-ui/barberCard/BarberCard.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import BarberPic from './BarberPic';
+import { theme } from '../../../theme';
+import { getFirstLetter } from '../../../utils/convert';
 
-export default function BarberCard() {
+export default function BarberCard({ name, isSelected, onClick }) {
+    console.log('isSelected', isSelected)
+
     return (
-        <BarberCardStyled>
-            Picture
-            Name
-            Input select
-        </BarberCardStyled>
+        <BarberCardStyled BarberCardStyled $isSelected={isSelected} onClick={onClick} >
+            <BarberPic letter={getFirstLetter(name)} />
+            <span className='name'>{name}</span>
+            <input className="radio" type="radio" checked={isSelected} />
+        </BarberCardStyled >
     )
 }
 const BarberCardStyled = styled.div`
-width: 50%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+border: 1px solid ${theme.colors.greyLight};
+box-sizing: border-box;
+padding: 5px 10px;
+margin: 0 5px;
+border-radius: ${theme.borderRadius.midRound};
+input {
+  accent-color: ${theme.colors.black};
+}
+
+${({ $isSelected }) => $isSelected && selectedStyle}
+
 `;
+
+const selectedStyle = css`
+border: 2px solid ${theme.colors.lowBlack};
+`
